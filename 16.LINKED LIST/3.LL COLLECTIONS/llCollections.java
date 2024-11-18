000b4bedebfef6c7e1a2858a99f26cd1aee3dc17
Aman-Raj-2508 <amansinghbhumihar2510@gmail.com>
2024-11-18 21:20:09 +0530

    Added collections

---
+++ 16.LINKED LIST/3.LL COLLECTIONS/llCollections.java
@@ -0,0 +1,38 @@
+
+import java.util.LinkedList;
+
+public class llCollections {
+
+    public static void main(String[] args) {
+
+        LinkedList<String> animals = new LinkedList<>();
+
+        animals.add("dog");// add funciton add elements at the end of Linked List
+        animals.add("horse");
+        animals.add("zebra");
+
+        System.out.println(animals);
+
+        animals.add(1, "cat"); // Adds at specific index
+
+        System.out.println(animals);
+
+        // many more add methods are available
+
+        System.out.println(animals.get(2)); // to retrieve from a index
+
+        animals.set(2, "lion"); // to replace the element at particular index
+        System.out.println(animals);
+
+        animals.remove(1); // Removes the element
+        System.out.println(animals);
+
+        System.out.println(animals.indexOf("lion")); // tells the first index where the lion is present
+
+        // To print all the elements
+        for (String animal : animals) {
+            System.out.println(animal);
+        }
+
+    }
+}
