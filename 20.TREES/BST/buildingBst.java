5f346101b25fae91cd427f308a336f08cf5bade3
Aman-Raj-2508 <amansinghbhumihar2510@gmail.com>
2025-02-28 10:00:01 +0530

    Added delete Node

--- 20.TREES/BST/buildingBst.java
+++ 20.TREES/BST/buildingBst.java
@@ -11,6 +11,7 @@ public class buildingBst {
         }
     }
 
+    // Insertion in BST
     public static Node insert(Node root, int val) {
         if (root == null) {
             root = new Node(val);
@@ -35,6 +36,42 @@ public class buildingBst {
         inorder(root.right);
     }
 
+    // Deletion in BST
+
+    public static Node delete(Node root, int val) {
+        if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else if (root.data > val) {
+            root.right = delete(root.right, val);
+        } else {
+            // Case-1 (Leaf Node)
+
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // Case-2 (One child)
+
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // Case-3 (Two child)
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
         int values[] = { 5, 1, 3, 4, 2, 7 };
         Node root = null;
@@ -42,6 +79,8 @@ public class buildingBst {
         for (int i = 0; i < values.length; i++) {
             root = insert(root, values[i]);
         }
+
+        root = delete(root, 1);
 
         inorder(root);
     }
